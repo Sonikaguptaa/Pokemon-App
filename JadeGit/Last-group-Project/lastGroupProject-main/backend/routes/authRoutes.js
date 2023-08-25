50ec02ff9f4dfb46b4bad539132b565ebd027e02
Sonika Gupta <133162380+Sonikaguptaa@users.noreply.github.com>
2023-08-25 16:50:25 -0600

    Delete JadeGit/Last-group-Project/lastGroupProject-main directory

--- JadeGit/Last-group-Project/lastGroupProject-main/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const authControl = require('../controllers/authController')
-
-router.post('/register', authControl.register)
-router.post('/login', authControl.login)
-
-module.exports = router
--- JadeGit/Last-group-Project/lastGroupProject-main/backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const userControl = require('../controllers/userController')
-
-router.get('/', userControl.show)
-
-module.exports = router
